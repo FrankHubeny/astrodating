de0fde07134ba5e12f0b6af48ecf7d25d2c7bf9f
Frank Hubeny <frankhubeny@protonmail.com>
2024-08-09 16:54:58 -0400

    initial files

--- vslpy/__init__.py
+++ vslpy/__init__.py
@@ -2,9 +2,12 @@
 
 __citation__ = 'Frank Hubeny'
 
-from . import _version
+#from . import _version
 #__version__ = 'v0.0.1-beta' #_version.get_versions()['version'] _version.get_versions()['version']
-__version__ = _version.get_versions()['version'] 
+#__version__ = _version.get_versions()['version'] 
 
+from setuptools_scm import get_version
+__version__ = version = get_version()
+	
 __all__ = ["", ""]
 
