1d43822361c5c2cf31aee94083c99ce8fcccaa4b
Frank Hubeny <frankhubeny@protonmail.com>
2024-08-13 17:31:01 -0400

    added mypy to meet sp-repo-review standards

---
+++ src/__init__.py
@@ -0,0 +1,12 @@
+# Licensed under a 3-clause BSD style license - see LICENSE
+
+__citation__ = "Frank Hubeny"
+
+# from . import _version
+# __version__ = 'v0.0.1-beta' #_version.get_versions()['version'] _version.get_versions()['version']
+# __version__ = _version.get_versions()['version']
+
+# from setuptools_scm import get_version
+# __version__ = version = get_version()
+
+__all__ = ["", ""]
---
+++ src/vsl.py
@@ -0,0 +1,45 @@
+# vsl.py
+"""The vsl class with its functions."""
+
+from astropy import constants as const
+
+
+class VSL:
+    """Class for testing package setup with documentation and automation.
+
+    This version is not part of the plan for the actual package.  It
+    only contains sample code to check the implication of pre-commits
+    and continuous integration.
+    """
+
+    def __init__(self: "VSL", name: str) -> None:
+        """Initialize the VSL clase."""
+        self.name = name
+
+    def __str__(self: "VSL") -> str:
+        """Return the string value."""
+        return self.name
+
+    # @u.quantity_input(speedfactor: u.dimensionless_unscaled)
+    def lightspeed(self: "VSL", speedfactor: float = 1.0) -> float:
+        """Return the speed of light based on a variable speed factor.
+
+        Parameters
+        ----------
+        speedfactor : float
+            The theoretically defined factor to obtain a speed of light.
+            For constant speed of light theories this factor is 1, the default.
+
+        Returns
+        -------
+        newlightspeed : float
+            The new light speed based on the constant and the speedfactor.
+
+        Examples
+        --------
+        >>> vsl.lightspeed()
+
+        """
+        newlightspeed = speedfactor * const.C
+        assert isinstance(newlightspeed, float)
+        return newlightspeed
